58fbcecedca4e2479120d3fc97936a0859ec0083
Manuel Garcia <manugarciautn@gmail.com>
2025-12-22 21:35:28 -0300

    cambio cors 4

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ import routerTask from './backend/routes/tasks.routes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const corsConfig = {
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5500','https://taskmanager-abpl.onrender.com','https://task-manager-swart-iota.vercel.app'], // dominios permitidos
-    methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],                  // métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'],          // cabeceras permitidas
-    exposedHeaders: ['Content-Length'],                         // cabeceras visibles al cliente
-    credentials: false,                                          // habilitar credenciales
-    maxAge: 600,                                                // cache preflight
-    optionsSuccessStatus: 204                                   // respuesta preflight exitosa
-}
+// const corsConfig = {
+//     origin: ['http://localhost:3000', 'http://127.0.0.1:5500','https://taskmanager-abpl.onrender.com','https://task-manager-swart-iota.vercel.app'], // dominios permitidos
+//     methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],                  // métodos permitidos
+//     allowedHeaders: ['Content-Type', 'Authorization'],          // cabeceras permitidas
+//     exposedHeaders: ['Content-Length'],                         // cabeceras visibles al cliente
+//     credentials: false,                                          // habilitar credenciales
+//     maxAge: 600,                                                // cache preflight
+//     optionsSuccessStatus: 204                                   // respuesta preflight exitosa
+// }
 
 
-app.use(cors(corsConfig));
+// app.use(cors(corsConfig));
+app.use(cors());
 app.use(express.json()); 
 
 app.use("/api",routerTask);
